b371e9969cae03ea0c1811c2723c91dcc5e46bf3
SudhirPawar123 <sudhipawar329@gmail.com>
2024-10-01 11:19:18 +0530

    added states of snake and ladder game

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static final int WINNING_POSITION = 100;
+    private static final int START_POSITION = 0;
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Welcome to snake and ladder game..!");
+
+
     }
 }
