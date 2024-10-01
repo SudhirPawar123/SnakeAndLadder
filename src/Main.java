f44dcdc1c0fda83e3f6fef1e305ed6dc3875ed0a
SudhirPawar123 <sudhipawar329@gmail.com>
2024-10-01 11:43:42 +0530

    added playSinglePlayer() function

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -16,9 +17,51 @@ public class Main {
         return options[random.nextInt(options.length)];
     }
 
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+    private static void playSinglePlayer() {
+        int position = START_POSITION;
+        int diceRollCount = 0;
+
+        while (position != WINNING_POSITION) {
+            System.out.println("Press Enter to roll the dice...");
+        new Scanner(System.in).nextLine();
+            int diceValue = rollDice();
+            diceRollCount++;
+            System.out.println("Rolled: " + diceValue);
+
+            String option = checkOption();
+            System.out.println("Option result: " + option);
+
+            int newPosition;
+            switch (option) {
+                case "No Play":
+                    newPosition = position;
+                    break;
+                case "Ladder":
+                    newPosition = position + diceValue;
+                    break;
+                case "Snake":
+                    newPosition = position - diceValue;
+                    break;
+                default:
+                    newPosition = position;
+            }
+            if (newPosition > WINNING_POSITION) {
+                System.out.println("Exceeded 100, staying at " + position);
+            } else if (newPosition < START_POSITION) {
+                System.out.println("Position below 0, restarting from 0");
+                position = START_POSITION;
+            } else {
+                position = newPosition;
+                System.out.println("Moved to position: " + position);
+            }
+        }
+
+        System.out.println("Player reached position 100!");
+        System.out.println("Total dice rolls: " + diceRollCount);
+    }
+
+
+            public static void main(String[] args) {
         System.out.printf("Welcome to snake and ladder game..!");
 
 
