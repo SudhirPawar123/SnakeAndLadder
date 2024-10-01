8cba645112eb75aaefd759d30fa6ceaff088fc40
SudhirPawar123 <sudhipawar329@gmail.com>
2024-10-01 11:27:08 +0530

    added rolldice function to take random dice number between 1 to 6

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
     private static final int WINNING_POSITION = 100;
     private static final int START_POSITION = 0;
+    private static Random random=new Random();
 
+    private static int rollDice() {
+        return random.nextInt(6) + 1;  // Roll the die (1 to 6)
+    }
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
